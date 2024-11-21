572d128f1e693ad7b4042a8285e8065399bb2e36
Lucas Sá <lucas.sodresa@gmail.com>
2024-11-21 07:16:49 +0000

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content Type Middleware', () => {
+  test('Should return default content type as json', async () => {
+    const routeName = '/test_content_type'
+    app.get(routeName, (req, res) => {
+      res.send()
+    })
+
+    await request(app).get(routeName)
+      .expect('content-type', /json/)
+  })
+  test('Should return xml content type when forced', async () => {
+    const routeName = '/test_content_type_xml'
+    app.get(routeName, (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app).get(routeName)
+      .expect('content-type', /xml/)
+  })
+})
