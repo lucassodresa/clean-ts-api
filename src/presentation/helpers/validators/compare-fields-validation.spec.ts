8932448986f91318fb46c337f28b27bbc9e7dc69
Lucas Sá <lucas.sodresa@gmail.com>
2024-11-30 09:08:45 +0000

    test: add tests CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,26 @@
+import { InvalidParamError } from '../../errors'
+import { ComapreFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): ComapreFieldsValidation => {
+  return new ComapreFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('ComapreFields Validation', () => {
+  test('Should return MissingParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'invalid_value'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+    expect(error).toBeFalsy()
+  })
+})
