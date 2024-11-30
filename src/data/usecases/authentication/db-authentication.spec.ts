a054ff8d319a4eb596a37971ed47f4dd9cff4b8a
Lucas Sá <lucas.sodresa@gmail.com>
2024-11-30 16:10:27 +0000

    test: ensure DbAuthentication throw if updateAccessTokenRepositoryStub throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -157,4 +157,11 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(fakeAuthentication)
     expect(updateSpy).toBeCalledWith(fakeAccount.id, 'any_token')
   })
+
+  test('Should throw if UpdateAccessTokenRepository throws', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut()
+    jest.spyOn(updateAccessTokenRepositoryStub, 'update').mockRejectedValueOnce(new Error('Error on HashComparer'))
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
 })
