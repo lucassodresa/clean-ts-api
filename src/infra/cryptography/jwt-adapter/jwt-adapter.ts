50da45de181a1e442184f275b47dfb652cf3e7a7
Lucas Sá <lucas.sodresa@gmail.com>
2024-11-30 19:46:24 +0000

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,13 @@
+import { Encrypter } from '../../../data/protocols/cryptography/encrypter'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements Encrypter {
+  constructor (
+    private readonly secret: string
+  ) {}
+
+  async encrypt (id: string): Promise<string> {
+    await jwt.sign({ id }, this.secret)
+    return null
+  }
+}
