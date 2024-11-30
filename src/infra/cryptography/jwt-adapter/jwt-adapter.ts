d2acdd67d5c3ec5e1a91118347fa6d78ebff9cec
Lucas Sá <lucas.sodresa@gmail.com>
2024-11-30 21:14:07 +0000

    refactor: remove duplicated code

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -2,9 +2,7 @@ import { Encrypter } from '../../../data/protocols/cryptography/encrypter'
 import jwt from 'jsonwebtoken'
 
 export class JwtAdapter implements Encrypter {
-  constructor (
-    private readonly secret: string
-  ) {}
+  constructor (private readonly secret: string) {}
 
   async encrypt (id: string): Promise<string> {
     const accessToken = await jwt.sign({ id }, this.secret)
