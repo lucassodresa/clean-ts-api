d51f36a66f9deda820ce63669461da977b7796e4
Lucas Sá <lucas.sodresa@gmail.com>
2024-12-04 21:49:05 +0000

    feat: ensure SingUpController returns an access token on sucess

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -16,17 +16,17 @@ export class SignUpController implements Controller {
       }
 
       const { name, email, password } = httpRequest.body
-      const account = await this.addAcccount.add({
+      await this.addAcccount.add({
         name,
         email,
         password
       })
-      await this.authentication.auth({
+      const accessToken = await this.authentication.auth({
         email,
         password
       })
 
-      return ok(account)
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
