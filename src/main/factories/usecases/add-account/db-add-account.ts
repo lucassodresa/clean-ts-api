4fa307b42e40923d1f66e2b38a22b44d8f852f56
Lucas Sá <lucas.sodresa@gmail.com>
2024-12-05 00:41:48 +0000

    feat: add missing depenency to DbAccount on makeDbAddAccount factory

--- src/main/factories/usecases/add-account/db-add-account.ts
+++ src/main/factories/usecases/add-account/db-add-account.ts
@@ -7,5 +7,5 @@ export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoDbRepository = new AccountMongoDbRepository()
-  return new DbAddAccount(bcryptAdapter, accountMongoDbRepository)
+  return new DbAddAccount(bcryptAdapter, accountMongoDbRepository, accountMongoDbRepository)
 }
