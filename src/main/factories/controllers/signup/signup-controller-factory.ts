8bc9619722c0f5d087d14a43ec1322bfdde83b32
Lucas Sá <lucas.sodresa@gmail.com>
2024-12-07 00:34:23 +0000

    refactor: rename files to keep consistency

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -2,7 +2,7 @@
 import { SignUpController } from '../../../../presentation/controllers/auth/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
-import { makeDbAddAccount } from '../../usecases/add-account/db-add-account'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
 import { makeSignUpValidation } from '../signup/signup-validation-factory'
 
