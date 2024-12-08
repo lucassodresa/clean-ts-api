2b158780c265530a3dd8ab043c34cb8018a075d3
Lucas Sá <lucas.sodresa@gmail.com>
2024-12-08 02:14:52 +0000

    chore: test trigger lefthook

--- src/infra/db/mongodb/survey/survey-mongodb-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongodb-repository.ts
@@ -5,6 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class SurveyMongoDbRepository implements AddSurveyRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
+
     await surveyCollection.insertOne(surveyData)
   }
 }
