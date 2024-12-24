929b1c4ce5d9f28867a4ebc5ae0d86ba6e245bc5
Lucas Sá <lucas.sodresa@gmail.com>
2024-12-24 13:55:29 +0000

    refactor: allow api to run locally or with docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongodb:27017/clean-ts-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-ts-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'warwrda3210op='
 }
