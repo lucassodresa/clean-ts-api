2a9c0b7e97f61ea449a2ec05bdd37a44bd63fde4
Lucas Sá <lucas.sodresa@gmail.com>
2024-12-24 13:57:10 +0000

    fix: make controllers return the error message instead the error name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -8,7 +8,7 @@ export default (app: Express): void => {
 
   const routesDirecttory = path.join(__dirname, '..', 'routes')
   readdirSync(routesDirecttory).map(async file => {
-    if (file.includes('.test.')) return
+    if (file.includes('.test.') || file.endsWith('.map')) return
 
     const filePath = path.join(routesDirecttory, file)
     const fileImported = await import(filePath)
