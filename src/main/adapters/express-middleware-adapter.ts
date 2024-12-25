e1d16930d60f26f2741020ec5798068685cf99a7
Lucas Sá <lucas.sodresa@gmail.com>
2024-12-25 21:58:43 +0000

    fix: bug on accountId assignment

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -8,10 +8,7 @@ export const adaptMiddleware = (middleware: Middleware) => {
     }
     const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
-      req.body = {
-        ...req.body,
-        ...httpResponse.body
-      }
+      Object.assign(req, httpResponse.body)
       next()
     } else {
       res.status(httpResponse.statusCode).json({
