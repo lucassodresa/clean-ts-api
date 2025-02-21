8c606d6b209977c83e440e876d7eeac81e804b00
Lucas Sá <lucas.sodresa@gmail.com>
2025-02-21 00:22:11 +0000

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

--- src/domain/usecases/survey-result/load-survey-result.ts
+++ src/domain/usecases/survey-result/load-survey-result.ts
@@ -1,5 +1,5 @@
 import { SurveyResultModel } from '@/domain/models/survey-result'
 
 export type LoadSurveyResult = {
-  save (surveyId: string): Promise<SurveyResultModel>
+  load (surveyId: string): Promise<SurveyResultModel>
 }
