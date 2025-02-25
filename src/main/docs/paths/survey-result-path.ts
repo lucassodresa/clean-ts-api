d3c8ac90b234747787dbf48942d1ec42fd165617
Lucas Sá <lucas.sodresa@gmail.com>
2025-02-25 20:34:40 +0000

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -1,5 +1,43 @@
 
 export const surveyResultPath = {
+  get: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['Survey'],
+    summary: 'Get survey answer',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
   put: {
     security: [
       {
@@ -35,41 +73,6 @@ export const surveyResultPath = {
             }
           }
         }
-      },
-      403: {
-        $ref: '#/components/forbidden'
-      },
-      404: {
-        $ref: '#/components/notFound'
-      },
-      500: {
-        $ref: '#/components/serverError'
-      }
-    }
-  },
-  post: {
-    security: [
-      {
-        apiKeyAuth: []
-      }
-    ],
-    tags: ['Survey'],
-    summary: 'Create new survey',
-    requestBody: {
-      content: {
-        'application/json': {
-          schema: {
-            $ref: '#/schemas/addSurveyParams'
-          }
-        }
-      }
-    },
-    responses: {
-      204: {
-        description: 'Success'
-      },
-      400: {
-        $ref: '#/components/badRequest'
       },
       403: {
         $ref: '#/components/forbidden'
