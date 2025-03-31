a3a2261bcb1adbee4d91763e12ddbf46d3c63a27
Lucas Sá <lucas.sodresa@gmail.com>
2025-03-31 22:41:37 +0100

    feat: setup apollo server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import { setupApolloServer } from './apollo-server'
 import { setupMiddlewares } from './middlewares'
 import { setupRoutes } from './routes'
 import { setupStaticFiles } from './static-files'
 import { setupSwagger } from './swagger'
 
 const app = express()
+setupApolloServer(app)
 setupSwagger(app)
 setupStaticFiles(app)
 setupMiddlewares(app)
