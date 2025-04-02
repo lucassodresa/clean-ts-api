ef049fd4f925421adaa83401e133ab8824c71fb8
Lucas Sá <lucas.sodresa@gmail.com>
2025-04-02 19:31:21 +0100

    test: ensure Signup Mutation returns an account on valid data

--- __tests__/main/graphql/login.test.ts
+++ __tests__/main/graphql/login.test.ts
@@ -22,6 +22,7 @@ describe('Auth GraphQL', () => {
     accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
+
   describe('Login Query', () => {
     const loginQuery = gql`
       query Login($email: String!, $password: String!) {
@@ -52,11 +53,38 @@ describe('Auth GraphQL', () => {
     test('Should return UnauthorizedError on invalid credentials', async () => {
       const { query } = createTestClient({ apolloServer })
       const response: any = await query(loginQuery, {
-        variables:
-        { email: 'any_email@mail.com', password: '123' }
+        variables: {
+          email: 'any_email@mail.com',
+          password: '123'
+        }
       })
       expect(response.data).toBeFalsy()
       expect(response.errors[0].message).toBe('Unauthorized')
+    })
+  })
+
+  describe('SignUp Query', () => {
+    const signUpMutation = gql`
+      mutation signup($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!) {
+        signup(name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation) {
+          accessToken
+          name
+        }
+      }
+    `
+
+    test('Should return an Account on valid data', async () => {
+      const { mutate } = createTestClient({ apolloServer })
+      const response: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        }
+      })
+      expect(response.data.signup.accessToken).toBeDefined()
+      expect(response.data.signup.name).toBe('any_name')
     })
   })
 })
