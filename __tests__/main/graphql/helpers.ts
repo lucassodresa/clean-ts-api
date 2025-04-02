9e6c487b6803606198c6e11de6080640a43983ee
Lucas Sá <lucas.sodresa@gmail.com>
2025-04-02 19:18:26 +0100

    test: ensure Login Query returns an account on valid credentials

---
+++ __tests__/main/graphql/helpers.ts
@@ -0,0 +1,10 @@
+import schemaDirectives from '@/main/graphql/directives'
+import resolvers from '@/main/graphql/resolvers'
+import typeDefs from '@/main/graphql/type-defs'
+import { ApolloServer } from 'apollo-server-express'
+
+export const makeApolloServer = (): ApolloServer => new ApolloServer({
+  resolvers,
+  typeDefs,
+  schemaDirectives
+})
---
+++ __tests__/main/graphql/login.test.ts
@@ -0,0 +1,52 @@
+import { MongoHelper } from '@/infra/db/mongodb'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { createTestClient } from 'apollo-server-integration-testing'
+import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
+import { makeApolloServer } from './helpers'
+
+let accountCollection: Collection
+let apolloServer: ApolloServer
+
+describe('Auth GraphQL', () => {
+  beforeAll(async () => {
+    apolloServer = makeApolloServer()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  describe('Login Query', () => {
+    const loginQuery = gql`
+      query Login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          accessToken
+          name
+        }
+      }
+    `
+
+    test('Should return an Account on valid credentials', async () => {
+      const password = 'any_password'
+      const passwordHashed = await hash(password, 12)
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: passwordHashed
+      })
+      const { query } = createTestClient({ apolloServer })
+      const response: any = await query(loginQuery, {
+        variables:
+        { email: 'any_email@mail.com', password }
+      })
+      expect(response.data.login.accessToken).toBeDefined()
+      expect(response.data.login.name).toBe('any_name')
+    })
+  })
+})
